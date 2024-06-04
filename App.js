3373e1e85cf38b564bdfdd96fb30103eaca2119f
bruno-martignago <brunomartignago106@gmail.com>
2024-06-04 20:23:04 -0300

    visor e botões finalizados

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import { Text, SafeAreaView, StyleSheet, View } from 'react-native';
 
 import Botao from "./src/components/Botao";
+import Visor from './src/components/Visor';
 
 export default function App() {
   return (
     <SafeAreaView style={estilos.container}>
+      <Visor />
       <View style={estilos.botoes}>
+      <Botao label ="AC" triplo />
+      <Botao label ="/" cor />
       <Botao label ="7"/>
       <Botao label ="8"/>
       <Botao label ="9"/>
-      <Botao label ="*"/>
+      <Botao label ="*" cor />
       <Botao label ="4"/>
       <Botao label ="5"/>
       <Botao label ="6"/>
-      <Botao label ="-"/>
+      <Botao label ="-" cor/>
       <Botao label ="1"/>
       <Botao label ="2"/>
       <Botao label ="3"/>
-      <Botao label ="+"/>
+      <Botao label ="+" cor />
+      <Botao label ="0" duplo />
+      <Botao label ="."/>
+      <Botao label ="=" cor />
       </View>
     </SafeAreaView>
   );
@@ -26,7 +33,7 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'flex-end',
   },
   botoes: {
     flexDirection: "row",
